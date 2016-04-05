b6995bfff8d1a8ad90823646cca3407ad6a08e68
Hane Liu <hane0131@gmail.com>
2016-04-05 11:58:46 +0800

    initial method forwarg

--- hw1.py
+++ hw1.py
@@ -9,5 +9,16 @@ class HMM:
 		self.ob_num = len(observation_list)
 		self.ob_prob = [[0 for j in range(self.ob_num)] for i in range(self.state_num)]	# Every state's observation probability
 
+	def forward(self, ob_list, time):
+		"""Use forward algorithm to evaluate probability of a given observation.
 
-		
+		Parameters
+		----------
+		ob_list : array-like, Observation list.
+		time : integer, Assign which time of observation list.
+
+		Returns
+		-------
+		p : float, Probability of given observation.
+		"""
+		ob_list = [self.ob_list.index(i) for i in ob_list]	# Transform observation to integer type
